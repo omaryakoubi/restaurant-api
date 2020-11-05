9ceca92c6d5febb99bd06bc4119b040d2070b310
omaryakoubi <yakoubiomar2001@gmail.com>
2020-11-05 17:14:20 +0100

    advancing

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const cors = require("cors");
+require("dotenv").config();
+
 const app = express();
+
+app.use(bodyParser.json());
+app.use(morgan("combined"));
+app.use(cors());
+
+const DB_URL = process.env.MONGODB_URL;
+
+mongoose.connect(DB_URL, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connected");
+});
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`server started on port ${PORT}`)
-})
+  console.log(`server started on port ${PORT}`);
+});
