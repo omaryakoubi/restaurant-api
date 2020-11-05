aaaaef6f04f29f9bc75fb6ced58457a8446ce8d7
omaryakoubi <yakoubiomar2001@gmail.com>
2020-11-05 17:53:57 +0100

    multer done

--- api/product/add_food.js
+++ api/product/add_food.js
@@ -1,13 +1,27 @@
 const router = require("express").Router();
-const multer = require("multer")
+const multer = require("multer");
 const food = require("../../models/food.js");
 
-router.post("add-food", async (req, res) => {
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "assets/food-images");
+  },
+
+  filename: (req, file, cb) => {
+    cb(null, `${new Date().toISOString()} ${file.originalname}`);
+  },
+});
+
+const upload = multer({ storage });
+
+router.post("add-food", upload.single("food_photo"), async (req, res) => {
   try {
     const { item_name, description } = req.body;
+    const { photo } = req.file.path;
     const new_food = await new food({
       item_name,
       description,
+      photo,
     }).save();
     res.status(201).send(new_food);
   } catch (error) {
